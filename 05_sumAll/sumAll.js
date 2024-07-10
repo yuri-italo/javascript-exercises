3ea068744a5b3ca5eee9bac8fa01136dc4f47485
yuri-italo <yuri.italo94@gmail.com>
2024-07-09 21:33:23 -0300

    add exercise 05_sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (num, num2) {
+  if (num < 0 || num2 < 0) return "ERROR";
+  if (typeof num !== "number" || typeof num2 !== "number") return "ERROR";
+  
+  let sum = 0;
+  if (num > num2) {
+    for (let i = num; i >= num2; i--) {
+      sum += i;
+    }
+  } else {
+    for (let i = num; i <= num2; i++) {
+      sum += i;
+    }
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
