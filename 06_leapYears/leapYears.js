2a831ea5ca26e1927ae28d093fa1b8b9f7672ec8
yuri-italo <yuri.italo94@gmail.com>
2024-07-10 22:24:55 -0300

    add exercise 06_leapYears solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if ((0 == year % 4 && 0 != year % 100) || 0 == year % 400) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
