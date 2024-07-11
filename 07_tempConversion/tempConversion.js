d55aed3988feb1e308a2c0ea5fda0e7fbe9451ab
yuri-italo <yuri.italo94@gmail.com>
2024-07-10 22:41:06 -0300

    add exercise 07_tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const celsius = (fahrenheit - 32) / 1.8;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const fahrenheit = celsius * 1.8 + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
