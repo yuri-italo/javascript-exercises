e5c4a810a9542e92a408d2ef46b7445fe216ab8d
yuri-italo <yuri.italo94@gmail.com>
2024-09-20 20:33:40 -0300

    feat: add exercise 08_calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((acc, cur) => acc * cur);
 };
 
-const power = function() {
-	
+const power = function (num, exponent) {
+  return num ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 0) return -1;
+  else if (num == 0) return 1;
+  else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
