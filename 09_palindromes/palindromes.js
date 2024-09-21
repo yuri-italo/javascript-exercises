713190426b82dee48f8aa314a62b5960c49b5b4f
yuri-italo <yuri.italo94@gmail.com>
2024-09-20 21:08:51 -0300

    feat: add exercise 09_palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const regex = /[^a-z0-9]/gi;
 
+  const noPunctuationString = str
+    .split("")
+    .filter((char) => !char.match(regex))
+    .join("")
+    .toLowerCase();
+
+  const reversed = noPunctuationString.split("").reverse().join("");
+
+  return noPunctuationString === reversed;
 };
 
 // Do not edit below this line
