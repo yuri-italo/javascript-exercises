ec2c113b30b89a046cd6fbdadb11bfc541ab8e0e
yuri-italo <yuri.italo94@gmail.com>
2024-09-21 09:55:06 -0300

    feat: add exercise 10_fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,6 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  return num < 1 ? 0 : num <= 2 ? 1 : fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
