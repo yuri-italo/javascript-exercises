75bfb94df3c5fe523a6a5b9b6316df086742ada0
yuri-italo <yuri.italo94@gmail.com>
2024-09-21 10:34:54 -0300

    feat: add exercise 12_findTheOldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let oldestPerson = {};
+  let oldestAge = 0;
 
+  arr.forEach((element) => {
+    if (!element.yearOfDeath) {
+      element.yearOfDeath = new Date().getFullYear();
+    }
+
+    const personAge = element.yearOfDeath - element.yearOfBirth;
+    if (personAge > oldestAge) {
+      oldestAge = personAge;
+      oldestPerson = element;
+    }
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
